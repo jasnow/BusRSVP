987de452b020100d2f9a498f9f84f26d10559be9
Al Snow <jasnow@hotmail.com>
2020-12-09 08:53:27 -0500

    Added version to migration files

--- db/migrate/20150906032601_create_trips_and_reservations.rb
+++ db/migrate/20150906032601_create_trips_and_reservations.rb
@@ -1,4 +1,4 @@
-class CreateTripsAndReservations < ActiveRecord::Migration
+class CreateTripsAndReservations < ActiveRecord::Migration[5.2]
   def change
     create_table :trips do |t|
       t.string :name
